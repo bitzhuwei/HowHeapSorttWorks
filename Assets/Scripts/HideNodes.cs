abee2dfe9199cd5476b136322f4e7741cc7cdc33
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-06-14 23:54:40 +0800

    hide nodes when running.

---
+++ Assets/Scripts/HideNodes.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideNodes : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            Transform child = this.transform.GetChild(i);
+            int tmp;
+            if (int.TryParse(child.name, out tmp))
+            {
+                child.renderer.enabled = false;
+                child.GetChild(0).renderer.enabled = false;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/LineManager.cs
+++ Assets/Scripts/LineManager.cs
@@ -9,14 +9,14 @@ public class LineManager : MonoBehaviour {
 	void Start () {
         if (nodePrefab != null)
         {
-            for (float i = -9, j = 0; i < 10; i += 1.2f, j++)
-            {
-                Transform node = Instantiate(nodePrefab) as Transform;
-                node.position = new Vector3(i, 0, 0);
-                TextMesh textMesh = node.GetComponentInChildren<TextMesh>();
-                textMesh.text = j.ToString();
-                node.name = j.ToString();
-            }
+            //for (float i = -9, j = 0; i < 10; i += 1.2f, j++)
+            //{
+            //    Transform node = Instantiate(nodePrefab) as Transform;
+            //    node.position = new Vector3(i, 0, 0);
+            //    TextMesh textMesh = node.GetComponentInChildren<TextMesh>();
+            //    textMesh.text = j.ToString();
+            //    node.name = j.ToString();
+            //}
         }
 	
 	}
