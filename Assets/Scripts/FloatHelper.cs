3af957dd644b3481ea9ace30b926bab5b7339c81
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-06-14 17:18:06 +0800

    prepare tree and line in mainScene.

---
+++ Assets/Scripts/FloatHelper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class FloatHelper
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static string ToShortString(this float value)
+    {
+        string result = string.Empty;
+
+        if(-10.0f < value && value < 10.0f)
+        {
+            result = string.Format("{0:0.00}", value);
+        }
+        else
+        {
+            result = string.Format("{0:0.0}", value);
+        }
+
+        return result;
+    }
+}
---
+++ Assets/Scripts/LineManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineManager : MonoBehaviour {
+
+    public Transform nodePrefab;
+
+	// Use this for initialization
+	void Start () {
+        if (nodePrefab != null)
+        {
+            for (float i = -9, j = 0; i < 10; i += 1.2f, j++)
+            {
+                Transform node = Instantiate(nodePrefab) as Transform;
+                node.position = new Vector3(i, 0, 0);
+                TextMesh textMesh = node.GetComponentInChildren<TextMesh>();
+                textMesh.text = j.ToString();
+                node.name = j.ToString();
+            }
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
