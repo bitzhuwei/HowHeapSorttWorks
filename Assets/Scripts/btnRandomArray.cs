6b18b8aaec6c0fe4dfabb0d19bcdf0384c96686a
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-06-18 14:28:24 +0800

    get random array.

---
+++ Assets/Scripts/btnRandomArray.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class btnRandomArray : MonoBehaviour
+{
+    public UnityEngine.UI.InputField txtArrayText;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void btnRandomArray_Click()
+    {
+        System.Text.StringBuilder builder = new System.Text.StringBuilder();
+        for (int i = 0; i < 15; i++)
+        {
+            int number = Random.Range(0, 100);
+            builder.Append(number);
+            builder.Append(", ");
+        }
+
+        this.txtArrayText.text = builder.ToString();
+    }
+}
